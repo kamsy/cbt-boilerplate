a85786bba867ac877134e129dc4d636a762947a2
kamsy <olekakamsy@gmail.com>
2020-07-04 04:45:25 +0100

    finished the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,47 @@
 import React from "react";
-import { url } from "../App";
-import { NavLink } from "react-router-dom";
+import { url, fakeAuth } from "../App";
+import { Menu, Dropdown } from "antd";
+import Avatar from "../assets/images/person.png";
+import PowerOffSvg from "../assets/svgs/PowerOffSvg";
+import { clear } from "../services/localStorageHelper";
+import CustomHistory from "../services/CustomHistory.js";
 
+const menu = (
+    <Menu>
+        <Menu.Item key="0">
+            <a href="#">Account</a>
+        </Menu.Item>
+
+        <Menu.Divider />
+        <Menu.Item
+            className="logout-btn"
+            key="3"
+            onClick={() => {
+                fakeAuth.signout();
+                clear();
+                CustomHistory.push({ pathname: url });
+                return window.location.reload();
+            }}>
+            <PowerOffSvg />
+            Logout
+        </Menu.Item>
+    </Menu>
+);
 const Header = () => {
-    return <div className="header">header</div>;
+    return (
+        <div className="header">
+            <ul className="header-list">
+                <Dropdown
+                    overlay={menu}
+                    trigger={["click"]}
+                    getPopupContainer={() => document.querySelector(".header")}>
+                    <span className="ant-dropdown-link">
+                        <img src={Avatar} alt="user avatar" />
+                    </span>
+                </Dropdown>
+            </ul>
+        </div>
+    );
 };
 
 export default Header;
