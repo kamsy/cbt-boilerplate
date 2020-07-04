9b46f4703921d8c75953cfdcc2592a56a528ff03
kamsy <olekakamsy@gmail.com>
2020-07-04 04:24:41 +0100

    edited build script, hopefully netlify shouldn pass now

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { url } from "../App";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+    return <div className="header">header</div>;
+};
+
+export default Header;
--- src/components/ProtectedLayout.js
+++ src/components/ProtectedLayout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../scss/protectedlayout.scss";
 import Sidebar from "./Sidebar";
+import Header from "./Header";
 
 export default ({ children }) => {
     return (
         <div className="protected-layout">
+            <Header />
             <Sidebar />
             <main className="main">{children}</main>
         </div>
